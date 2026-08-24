0ff9d74ca297073dde2d58bb1c71a28cc2eeb257
princetnwr723-cloud <princetnwr723@gmail.com>
2026-08-24 10:17:16 +0530

    Update BusinessDNASection.tsx

--- components/BusinessDNASection.tsx
+++ components/BusinessDNASection.tsx
@@ -314,7 +314,7 @@ export default function BusinessDNASection({ workspaceId }: { workspaceId: strin
 
                 {briefing.motivationalNote && (
                   <div style={{ fontSize: 12, color: "#555", fontStyle: "italic", textAlign: "center", paddingTop: 10 }}>
-                    {briefing.motivationalNote} 👾
+                    {briefing.motivationalNote} 👹
                   </div>
                 )}
               </div>
